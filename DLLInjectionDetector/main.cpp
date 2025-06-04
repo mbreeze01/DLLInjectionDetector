3e6022402e7223455d4c74f7a1aa2bc03c9818ce
Fatmike-GH <Fatmike_Github@gmx.net>
2025-06-04 17:04:29 +0200

    Added commandline

--- DLLInjectionDetector/main.cpp
+++ DLLInjectionDetector/main.cpp
@@ -3,15 +3,37 @@
 #include "InjectionMonitor\InjectionMonitor.h"
 #include "InjectionGuard\InjectionGuard.h"
 
-int main()
+void ShowInfo();
+void ShowHelp();
+
+int wmain(int argc, wchar_t* argv[])
 {
-  std::wcout << "DLL Injection Detector." << std::endl;
-  
-  //InjectionDetector::InjectionDetector::Instance()->Initialze(new InjectionDetector::InjectionMonitor());
+  if (argc != 2)
+  {
+    ShowHelp();
+    return 0;
+  }
 
-  InjectionDetector::InjectionDetector::Instance()->Initialze(new InjectionDetector::InjectionGuard());
+  LPWSTR parameter = argv[1];
+  if (wcscmp(parameter, L"-m") == 0)
+  {
+    ShowInfo();
+    std::wcout << "Starting in monitoring mode." << std::endl;
+    InjectionDetector::InjectionDetector::Instance()->Initialze(new InjectionDetector::InjectionMonitor());
+  }
+  else if (wcscmp(parameter, L"-g") == 0)
+  {
+    ShowInfo();
+    std::wcout << "Starting in guard mode." << std::endl;
+    InjectionDetector::InjectionDetector::Instance()->Initialze(new InjectionDetector::InjectionGuard());
+  }
+  else
+  {
+    ShowHelp();
+    return 0;
+  }
 
-  std::wcout << "Waiting for injection" << std::endl;
+  std::wcout << std::endl << "Waiting for injection";
 
   while (true)
   {
@@ -19,3 +41,20 @@ int main()
     std::wcout << ".";
   }
 }
+
+void ShowInfo()
+{
+  std::wcout << std::endl;
+  std::wcout << "..::[Fatmike 2025]::.." << std::endl << std::endl;
+  std::wcout << "Version: DLL Injection Detector 0.0.1" << std::endl;
+}
+
+void ShowHelp()
+{
+  ShowInfo();
+  std::wcout << "Usage:\t DLLInjectionDetector.exe -m " << " : Start in monitoring mode (monitoring only)" << std::endl;
+  std::wcout << "Usage:\t DLLInjectionDetector.exe -g " << " : Start in guard mode (blocking dll injections)" << std::endl << std::endl;
+  std::wcout << "Press enter to exit." << std::endl;
+  std::wcin.get();
+}
+
